e453cd7f8307286e3eafe647ae458e8977c986d4
0x00001FFF <just.impovegg@gmail.com>
2025-03-07 18:22:14 -0300

    ref/ Add Custom query to fetch active permissions by id
    At first, I implemented a simple findByIdIn query, but then I remembered
    that roles can only have active permissions associated with them, so I
    refactored the query and made it with JPQL so I could keep the method name
    and change the return value.

--- src/main/java/dev/sunless/auth_api/repositories/RoleRepository.java
+++ src/main/java/dev/sunless/auth_api/repositories/RoleRepository.java
@@ -1,8 +1,11 @@
 package dev.sunless.auth_api.repositories;
 
+import dev.sunless.auth_api.models.Permission;
 import dev.sunless.auth_api.models.Role;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -10,4 +13,6 @@ public interface RoleRepository extends BaseRepository<Role, UUID>{
 
     Boolean existsByName(String name);
 
+    @Query("SELECT * FROM Permission p WHERE p.id IN :ids AND p.deletedAt IS NULL")
+    List<Permission> findByIdIn(List<UUID> id);
 }
