20e1f7716f4ab3b65bae219533fbf46133e312af
0x00001FFF <just.impovegg@gmail.com>
2025-03-08 19:02:03 -0300

    ref/ Refactor RoleMapper only displays active permissions

--- src/main/java/dev/sunless/auth_api/mappers/PermissionMapper.java
+++ src/main/java/dev/sunless/auth_api/mappers/PermissionMapper.java
@@ -6,19 +6,28 @@ import dev.sunless.auth_api.dtos.response.PermissionResponseDto;
 import dev.sunless.auth_api.models.Permission;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.ERROR)
 public abstract class PermissionMapper {
 
-    @Mapping(target = "name", source = "name")
+    @Mapping(target = "name", source = "dto.name")
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "lastModified", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    @Mapping(target = "id", ignore = true)
     public abstract Permission toModel(PermissionRequestDto dto);
 
     @Mapping(target = "id", source = "permission.id")
-    @Mapping(target = "createdDate", source = "createdDate")
+    @Mapping(target = "createdDate", source = "permission.createdDate")
+    @Mapping(target = "lastModified", source = "permission.lastModified")
     public abstract PermissionResponseDto toResponse(Permission permission);
 
     @Mapping(target = "id", source = "permission.id")
     @Mapping(target = "name", source = "permission.name")
     @Mapping(target = "deletedAt", source = "permission.deletedAt")
+    @Mapping(target = "createdDate", source = "permission.createdDate")
+    @Mapping(target = "lastModified", source = "permission.lastModified")
     public abstract InactivePermissionResponseDto toInactiveResponse(Permission permission);
 }
--- src/main/java/dev/sunless/auth_api/mappers/RoleMapper.java
+++ src/main/java/dev/sunless/auth_api/mappers/RoleMapper.java
@@ -2,34 +2,58 @@ package dev.sunless.auth_api.mappers;
 
 import dev.sunless.auth_api.dtos.request.RoleRequestDto;
 import dev.sunless.auth_api.dtos.response.InactiveRoleResponseDto;
+import dev.sunless.auth_api.dtos.response.PermissionResponseDto;
 import dev.sunless.auth_api.dtos.response.RoleResponseDto;
 import dev.sunless.auth_api.models.Permission;
 import dev.sunless.auth_api.models.Role;
-import dev.sunless.auth_api.repositories.PermissionRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
+import java.util.stream.Collectors;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.ERROR)
 public abstract class RoleMapper {
 
     @Autowired
-    private PermissionRepository permissionRepository;
+    private PermissionMapper permissionMapper;
 
-    @Mapping(target = "permissions", source = "role.permissions")
+    @Mapping(target = "permissions",
+            expression = """
+                    java(
+                        filterActivePermissions(role.getPermissions())
+                    )
+                    """)
+    @Mapping(target = "createdDate", source = "role.createdDate")
+    @Mapping(target = "lastModified", source = "role.lastModified")
+    @Mapping(target = "id", source = "role.id")
     public abstract RoleResponseDto toResponse(Role role);
 
     @Mapping(target = "permissions", source = "permissions")
+    @Mapping(target = "name", source = "requestDto.name")
+    @Mapping(target = "description", source = "requestDto.description")
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "lastModified", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    @Mapping(target = "id", ignore = true)
     public abstract Role toModel(RoleRequestDto requestDto, Set<Permission> permissions);
 
-    @Mapping(target = "deletedAt", source = "deletedAt")
-    @Mapping(target = "createdDate", source = "createdDate")
-    @Mapping(target = "lastModified", source = "lastModified")
+    @Mapping(target = "deletedAt", source = "role.deletedAt")
+    @Mapping(target = "createdDate", source = "role.createdDate")
+    @Mapping(target = "lastModified", source = "role.lastModified")
+    @Mapping(target = "id", source = "role.id")
+    @Mapping(target = "name", source = "role.name")
     public abstract InactiveRoleResponseDto toInactiveResponse(Role role);
+
+    protected Set<PermissionResponseDto> filterActivePermissions(Set<Permission> permissions) {
+        return permissions
+                .stream()
+                .filter(permission -> permission.getDeletedAt() == null)
+                .map(permission -> permissionMapper.toResponse(permission))
+                .collect(Collectors.toSet());
+    }
 
 }
