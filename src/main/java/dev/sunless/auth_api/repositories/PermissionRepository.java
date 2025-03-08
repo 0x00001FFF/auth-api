a1393c280e8d83f38137458da27829d438600ed2
0x00001FFF <just.impovegg@gmail.com>
2025-03-08 11:59:06 -0300

    fix/ Fixed PermissionRepository JPQL query
    JPQL queries for entities so you cannot use wild cards like *, and I
    also put the query on the wrong repository, quick fix though

--- src/main/java/dev/sunless/auth_api/repositories/PermissionRepository.java
+++ src/main/java/dev/sunless/auth_api/repositories/PermissionRepository.java
@@ -1,13 +1,20 @@
 package dev.sunless.auth_api.repositories;
 
 import dev.sunless.auth_api.models.Permission;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface PermissionRepository extends BaseRepository<Permission, UUID>{
 
     Boolean existsByName(String name);
+
+    @Query("SELECT p FROM Permission p WHERE p.id IN :ids AND p.deletedAt IS NULL")
+    List<Permission> findByIdIn(@Param("ids") Set<UUID> permissionIds);
 
 }
