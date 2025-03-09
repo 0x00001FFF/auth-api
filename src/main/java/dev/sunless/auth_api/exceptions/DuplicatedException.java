335bfadd63945709dcfcd5c6b7ce052225b4b9f1
0x00001FFF <just.impovegg@gmail.com>
2025-03-09 14:59:53 -0300

    style/ Changing the duplicate exception message

--- src/main/java/dev/sunless/auth_api/exceptions/DuplicatedException.java
+++ src/main/java/dev/sunless/auth_api/exceptions/DuplicatedException.java
@@ -2,10 +2,10 @@ package dev.sunless.auth_api.exceptions;
 
 public class DuplicatedException extends RuntimeException {
     public DuplicatedException(String message) {
-        super("%s already is registered in the database".formatted(message));
+        super("%s is already registered in the database".formatted(message));
     }
 
     public DuplicatedException(String message, Throwable cause) {
-        super("%s already is registered in the database".formatted(message), cause);
+        super("%s is already registered in the database".formatted(message), cause);
     }
 }
